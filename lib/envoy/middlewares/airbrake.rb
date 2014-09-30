f857fdaa70f9c5ffaf58df79a3895040d6a34fd6
Lee Henson <lee.m.henson@gmail.com>
2014-09-30 16:26:56 +0100

    cache headers in airbrake middleware to avoid DAE in rescue block

--- lib/envoy/middlewares/airbrake.rb
+++ lib/envoy/middlewares/airbrake.rb
@@ -3,13 +3,13 @@ module Envoy
     class Airbrake
       def initialize app, worker
         @app = app
-        @worker = worker
+        @headers = @worker.message.headers
       end
 
       def call env
         @app.call env
       rescue => e
-        ::Airbrake.notify_or_ignore e, parameters: { message: @worker.message.headers }
+        ::Airbrake.notify_or_ignore e, parameters: { message: @headers }
         raise e
       end
     end
